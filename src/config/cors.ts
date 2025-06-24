e234ea8d7cd5fa45a8e52d479f7c2c9f82750b9f
logicalHassan <hassandev404@gmail.com>
2025-06-24 12:00:00 +0500

    fixed cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,5 @@ const allowedOrigins = process.env.NODE_ENV === 'production' ? env.frontend.url 
 
 export const corsOptions: CorsOptions = {
   origin: allowedOrigins,
-  credentials: true,
-  // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  // allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
 };
